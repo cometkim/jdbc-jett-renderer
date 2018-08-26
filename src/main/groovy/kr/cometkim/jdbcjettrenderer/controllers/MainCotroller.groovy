b1254a4a5799cc6b74b4fcd3751dac7d052a4961
Hyeseong Kim <cometkim.kr@gmail.com>
2018-08-27 06:26:22 +0900

    JDBC Connection 처리 관련 폼/컨트롤러 기능 추가
    - JDBC Connection 폼 인풋 추가
    - JDBC Connection 으로부터 메타데이터 추출하는 코드 추가

--- src/main/groovy/kr/cometkim/jdbcjettrenderer/controllers/MainCotroller.groovy
+++ src/main/groovy/kr/cometkim/jdbcjettrenderer/controllers/MainCotroller.groovy
@@ -1,8 +1,10 @@
 package kr.cometkim.jdbcjettrenderer.controllers
 
+import net.sf.jett.jdbc.JDBCExecutor
 import net.sf.jett.transform.ExcelTransformer
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -11,26 +13,97 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
 
+import java.sql.DatabaseMetaData
+import java.sql.DriverManager
+
 @Controller
 class MainCotroller {
     @GetMapping('/')
     public String index() { 'index' }
 
     @PostMapping('/render')
-    public ResponseEntity<ByteArrayResource> renderTemplate(@RequestParam("template") MultipartFile template) {
-        def fileIn = template.getInputStream()
-        def fileOut = new ByteArrayOutputStream()
+    public ResponseEntity<ByteArrayResource> renderTemplate(
+        @RequestParam('host') String host,
+        @RequestParam('port') Integer port,
+        @RequestParam('database') String dbname,
+        @RequestParam('username') String username,
+        @RequestParam('password') String password,
+        @RequestParam('template') MultipartFile template
+    ) {
+        def conn = null
+        def fileOut = null
+        try {
+            conn = DriverManager.getConnection(
+                "jdbc:mysql//${host}:${port}/${dbname}",
+                username,
+                password,
+            )
+            def jdbc = new JDBCExecutor(conn)
+            def metadata = conn.getMetaData()
+            def tables = getTableInfoList(metadata)
 
-        def context = [:]
+            def ctx = [
+                jdbc  : jdbc,
+                tables: tables,
+            ]
 
-        new ExcelTransformer()
-            .transform(fileIn, context)
-            .write(fileOut)
+            def fileIn = template.getInputStream()
+            fileOut = new ByteArrayOutputStream()
 
-        ResponseEntity
-            .ok()
-            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"${template.originalFilename}\"")
-            .body(new ByteArrayResource(fileOut.toByteArray()))
+            new ExcelTransformer()
+                .transform(fileIn, ctx)
+                .write(fileOut)
+
+            ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"${template.originalFilename}\"")
+                .body(new ByteArrayResource(fileOut.toByteArray()))
+        } catch (Exception e) {
+            ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(e.getStackTrace())
+        } finally {
+            conn?.close()
+            fileOut?.close()
+        }
+    }
+
+    private List<Object> getTableInfoList(DatabaseMetaData metadata) {
+        def tableInfoList = [[:]]
+
+        def tableMetadata = metadata.getTables(null, null, null, ['TABLE'])
+        while(tableMetadata.next()) {
+            def tableName = tableMetadata.getString('TABLE_NAME')
+            def columns = getColumnInfoList(metadata, tableName)
+
+            // TODO: Get more information from ResultSet and Push it
+            def tableInfo = [
+                name: tableName,
+                columns: columns,
+            ]
+            tableInfoList += tableInfo
+        }
+
+        return tableInfoList
+    }
+
+    private List<Object> getColumnInfoList(DatabaseMetaData metadata, String tableName) {
+        def columnInfoList = [[:]]
+
+        def columnMetadata = metadata.getColumns(null, null, tableName, null)
+        while (columnMetadata.next()) {
+            def columnName = columnMetadata.getString('COLUMN_NAME')
+
+            // TODO: Get information from ResultSet and Push it
+            def columnInfo = [
+                name: columnName,
+            ]
+            columnInfoList += columnInfo
+        }
+
+        return columnInfoList
     }
 }
+
