f35411f51ad2268f9d8efcc58bcc3d44dae04956
Hyeseong Kim <cometkim.kr@gmail.com>
2018-08-27 04:44:20 +0900

    엑셀 템플릿 렌더링 컨트롤러 구현
    - JETT 라이브러리 종속성 추가
    - Form 코드 수정
    - POST /render 라우트 추가

--- src/main/groovy/kr/cometkim/jdbcjettrenderer/controllers/MainCotroller.groovy
+++ src/main/groovy/kr/cometkim/jdbcjettrenderer/controllers/MainCotroller.groovy
@@ -1,10 +1,36 @@
 package kr.cometkim.jdbcjettrenderer.controllers
 
+import net.sf.jett.transform.ExcelTransformer
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 
 @Controller
 class MainCotroller {
     @GetMapping('/')
     public String index() { 'index' }
+
+    @PostMapping('/render')
+    public ResponseEntity<ByteArrayResource> renderTemplate(@RequestParam("template") MultipartFile template) {
+        def fileIn = template.getInputStream()
+        def fileOut = new ByteArrayOutputStream()
+
+        def context = [:]
+
+        new ExcelTransformer()
+            .transform(fileIn, context)
+            .write(fileOut)
+
+        ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"${template.originalFilename}\"")
+            .body(new ByteArrayResource(fileOut.toByteArray()))
+    }
 }
