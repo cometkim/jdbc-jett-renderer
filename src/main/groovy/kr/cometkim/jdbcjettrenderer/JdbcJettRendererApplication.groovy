dda4d957d3a8be24bf42f607a5feca6edabaeda3
Hyeseong Kim <cometkim.kr@gmail.com>
2018-08-27 03:31:35 +0900

    Initial commit with Spring Boot Initializer

---
+++ src/main/groovy/kr/cometkim/jdbcjettrenderer/JdbcJettRendererApplication.groovy
@@ -0,0 +1,12 @@
+package kr.cometkim.jdbcjettrenderer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JdbcJettRendererApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run JdbcJettRendererApplication, args
+    }
+}
---
+++ src/test/groovy/kr/cometkim/jdbcjettrenderer/JdbcJettRendererApplicationTests.groovy
@@ -0,0 +1,16 @@
+package kr.cometkim.jdbcjettrenderer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class JdbcJettRendererApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
